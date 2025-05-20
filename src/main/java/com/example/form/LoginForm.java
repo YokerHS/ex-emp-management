be40e4e66c4d02ccb145d23341441a980e385111
YokerHS <hiimyoker@gmail.com>
2025-05-20 14:30:41 +0900

    formの修正

--- src/main/java/com/example/form/LoginForm.java
+++ src/main/java/com/example/form/LoginForm.java
@@ -1,4 +1,30 @@
 package com.example.form;
 /** ログイン時に使うフォーム */
 public class LoginForm {
+    private String mailAddress;
+    private String password;
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginForm{" +
+                "mailAddress='" + mailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
