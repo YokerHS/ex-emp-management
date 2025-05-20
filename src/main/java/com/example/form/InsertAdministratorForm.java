2822c81d29426096187e345038aaf85b07d7e495
YokerHS <hiimyoker@gmail.com>
2025-05-20 13:34:56 +0900

    Insert Admin form を定義する

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,4 +1,40 @@
 package com.example.form;
 /** 管理者登録時に使うフォーム */
 public class InsertAdministratorForm {
+    private String name;
+    private String mailAddress;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "InsertAdministratorForm{" +
+                "name='" + name + '\'' +
+                ", mailAddress='" + mailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,4 +1,17 @@
 package com.example.service;
+
+import com.example.repository.AdministratorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /** 管理者関連機能の業務処理を行うサービス */
+@Service
+@Transactional
 public class AdministratorService {
+
+    @Autowired
+    private AdministratorRepository administratorRepository;
+
+
 }
