7e8e4db21e9b5bdd6efd3a4ea6589cb63693c571
YokerHS <hiimyoker@gmail.com>
2025-05-20 12:03:16 +0900

    Addministrator domain を追加

--- src/main/java/com/example/domain/Administrator.java
+++ src/main/java/com/example/domain/Administrator.java
@@ -1,5 +1,63 @@
 package com.example.domain;
 /**管理者情報を表すドメインクラス
  * administrators　テーブルに対応  */
+
 public class Administrator {
+    /** ID （主キー） */
+    private Integer id;
+
+    /** 名前 */
+    private String name;
+
+    /** メールアドレス */
+    private String mailAddress;
+
+    /** パスワード */
+    private String password;
+
+
+    /**
+     * Getter, Setter
+     * */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail_address() {
+        return mail_address;
+    }
+
+    public void setMail_address(String mail_address) {
+        this.mail_address = mail_address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mail_address='" + mail_address + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
